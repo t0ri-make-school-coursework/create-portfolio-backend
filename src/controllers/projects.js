a372227a597f36ab5318958c8acc5276eed89b9a
t0ri <victoriarosemurray@gmail.com>
2019-03-04 20:39:02 -0800

    update works, abt to fiddle with create

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -3,14 +3,14 @@ const Project = require('../models/project');
 
 module.exports = (app) => {
   // Create New Project
-  app.post('/projects/new', (req, res) => {
-    const project = new Project(req.body);
+  // app.post('/projects/new', (req, res) => {
+  //   const project = new Project(req.body);
 
-    project.save(newguy =>
-      res.send(newguy))
-      .catch(err =>
-        new Error(err.message));
-  });
+  //   project.save(project =>
+  //     res.send(project))
+  //     .catch(err =>
+  //       new Error(err.message));
+  // });
 
   // Get All Projects
   app.get('/projects', (req, res) => {
@@ -37,15 +37,19 @@ module.exports = (app) => {
   // Remove A Project
   app.delete('/projects/:id', (req, res) => {
     Project.findByIdAndRemove({ _id: req.params.id })
-      .then(() => {
-        // check if it's deleted
-        Project.findById(req.params.id)
-          .then((project) => {
-            res.send(project);
-          })
-          .catch((err) => {
-            new Error(err.message);
-          });
+      .then(() => res.sendStatus(200))
+      .catch((err) => {
+        new Error(err.message);
+      });
+  });
+
+  // Update A Project
+  app.put('/projects/:id/edit', (req, res) => {
+    Project.findByIdAndUpdate(req.params.id, req.body)
+      .then(() => res.sendStatus(200))
+      .catch((err) => {
+        const error = new Error(err.message);
+        console.log(error);
       });
   });
 };
